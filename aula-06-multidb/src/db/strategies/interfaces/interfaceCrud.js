bb0a50d0e06f38d0de6ef79c6b384dcfdde65f7b
João Pedro Basso Audibert <hotsjoao@gmail.com>
2019-08-10 17:35:58 -0300

    configuring sequelize (committing everything)

--- aula-06-multidb/src/db/strategies/interfaces/interfaceCrud.js
+++ aula-06-multidb/src/db/strategies/interfaces/interfaceCrud.js
@@ -14,6 +14,10 @@ class ICrud {
   delete(id) {
     throw new NotImplementedException()
   }
+
+  isConnected(){
+    throw new NotImplementedException() 
+  }
 }
 
 module.exports = ICrud
--- aula-06-multidb/src/db/strategies/postgres.js
+++ aula-06-multidb/src/db/strategies/postgres.js
@@ -1,8 +1,59 @@
 const ICrud = require('./interfaces/interfaceCrud')
+const Sequelize = require('sequelize');
 
 class Postgres extends ICrud {
   constructor() {
     super()
+    this._driver = null
+    this._herois = null
+    this._connect()
+  }
+
+  _connect() {
+    this._driver = new Sequelize(
+      'heroes', // DB
+      'joaopedro', // Login
+      'joaopedro', { // Senha
+        host: 'localhost',
+        dialect: 'postgres',
+        quoteIdentifiers: false,
+      }
+    )
+  }
+
+  async defineModel(){
+    this._herois = driver.define('herois', {
+      id: {
+        type: Sequelize.INTEGER,
+        required: true,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      nome: {
+        type: Sequelize.STRING,
+        required: true,
+      },
+      poder: {
+        type: Sequelize.STRING,
+        required: true,
+      },
+    }, {
+        tableName: 'TB_HEROIS',
+        freezeTableName: false,
+        timestamps: false,
+      })
+  
+    await Herois.sync()
+  }
+
+  async isConnected(){
+    try {
+      await this._driver.authenticate()
+      return true
+    } catch (error) {
+      console.error('FAIL...', error);
+      return false;
+    }
   }
 
   create(item) {

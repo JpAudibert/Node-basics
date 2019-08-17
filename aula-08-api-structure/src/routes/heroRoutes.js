0cf4ed7e17f7fab20d50ab4308f12ab58a71b198
João Pedro Basso Audibert <hotsjoao@gmail.com>
2019-08-17 00:32:37 -0300

    Creating the listing system of the inserted heroes

--- aula-08-api-structure/src/routes/heroRoutes.js
+++ aula-08-api-structure/src/routes/heroRoutes.js
@@ -11,7 +11,26 @@ class HeroRoutes extends BaseRoute {
       path: '/herois',
       method: 'GET',
       handler: (request, headers) => {
-        return this.db.read()
+        try {
+          const { skip, limit, nome } = request.query
+
+          let query = {}
+          if (nome) {
+            query.nome = nome
+          }
+
+          if (isNaN(skip))
+            throw Error('O tipo do skip é incorreto')
+          if (isNaN(limit))
+            throw Error('O tipo do limit é incorreto')
+
+          return this.db.read(query, parseInt(skip), parseInt(limit))
+
+        } catch (error) {
+          console.error('Deu Ruim', error);
+          return 'Erro interno no servidor'
+
+        }
       }
     }
 
--- aula-08-api-structure/src/tests/api-heroes.test.js
+++ aula-08-api-structure/src/tests/api-heroes.test.js
@@ -10,13 +10,38 @@ describe('Teste da API Heroes', function () {
   it('Listar /herois', async () => {
     const result = await app.inject({
       method: 'GET',
-      url: '/herois'
+      url: '/herois?skip=0&limit=10'
+    })
+
+    const statusCode = result.statusCode
+
+    assert.deepEqual(statusCode, 200)
+  })
+
+  it('Listar /herois - deve retornar um erro com limit incorreto', async () => {
+    const TAMANHO_LIMITE = 'AEee'
+    const result = await app.inject({
+      method: 'GET',
+      url: `/herois?skip=0&limit=${TAMANHO_LIMITE}`
+    })
+
+    assert.deepEqual(result.payload, 'Erro interno no servidor')
+
+  })
+
+  it('Listar /herois - deve filtrar 1 item', async () => {
+    const TAMANHO_LIMITE = 1000
+    const NAME = `Homem Aranha - 1566010552951`
+    const result = await app.inject({
+      method: 'GET',
+      url: `/herois?skip=0&limit=${TAMANHO_LIMITE}&nome=${NAME}`
     })
 
     const dados = JSON.parse(result.payload)
     const statusCode = result.statusCode
 
-     assert.deepEqual(statusCode, 200)
-    assert.ok(Array.isArray(dados))
+    assert.deepEqual(statusCode, 200)
+    assert.ok(dados[0].nome === NAME)
+
   })
 })
